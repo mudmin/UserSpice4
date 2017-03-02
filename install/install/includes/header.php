e6788be8482aa61f5a18011617e9e88c4ad37dcc
Dan Hoover <mudmin@gmail.com>
2017-03-02 14:37:25 -0900

    UserSpice 4.2.1f Minor Bugfix on Installer

--- install/install/includes/header.php
+++ install/install/includes/header.php
@@ -19,7 +19,6 @@
                <link rel="stylesheet" href="install/css/bootstrap-theme.min.css">
                <link rel="stylesheet" href="install/css/main.css">
 
-               <script src="install/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
            </head>
            <body>
  <?php
