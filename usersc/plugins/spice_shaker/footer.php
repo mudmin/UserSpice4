d4c42da50f1e0c9123acd37ff9fc4686f09818a1
Dan Hoover <mudmin@gmail.com>
2019-05-24 17:40:09 -0400

    Spice Shaker!

---
+++ usersc/plugins/spice_shaker/footer.php
@@ -0,0 +1,3 @@
+<?php
+//Please don't load code on the footer of every page if you don't need it on the footer of every page.
+//bold("<br>Spice Shaker Footer Loaded");
---
+++ usersc/plugins/spice_shaker/install.php
@@ -0,0 +1,45 @@
+<?php
+require_once("init.php");
+//For security purposes, it is MANDATORY that this page be wrapped in the following
+//if statement. This prevents remote execution of this code.
+if (in_array($user->data()->id, $master_account)){
+
+
+$db = DB::getInstance();
+include "plugin_info.php";
+
+
+
+//all actions should be performed here.
+$check = $db->query("SELECT * FROM us_plugins WHERE plugin = ?",array($plugin_name))->count();
+if($check > 0){
+	err($plugin_name.' has already been installed!');
+}else{
+ $fields = array(
+	 'plugin'=>$plugin_name,
+	 'status'=>'installed',
+ );
+ $db->insert('us_plugins',$fields);
+ if(!$db->error()) {
+	 	err($plugin_name.' installed');
+		logger($user->data()->id,"USPlugins",$plugin_name." installed");
+ } else {
+	 	err($plugin_name.' was not installed');
+		logger($user->data()->id,"USPlugins","Failed to to install plugin, Error: ".$db->errorString());
+ }
+}
+$db->query("ALTER TABLE settings ADD COLUMN spice_api varchar(255)");
+//do you want to inject your plugin in the middle of core UserSpice pages?
+$hooks = [];
+
+//The format is $hooks['userspicepage.php']['position'] = path to filename to include
+//Note you can include the same filename on multiple pages if that makes sense;
+//postion options are post,body,form,bottom
+//See documentation for more information
+// $hooks['login.php']['body'] = 'hooks/loginbody.php';
+// $hooks['login.php']['form'] = 'hooks/loginform.php';
+// $hooks['login.php']['bottom'] = 'hooks/loginbottom.php';
+// $hooks['login.php']['post'] = 'hooks/loginpost.php';
+registerHooks($hooks,$plugin_name);
+
+} //do not perform actions outside of this statement
